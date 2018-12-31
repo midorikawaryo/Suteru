fc49bca9b35502f1066d85e66ef8130a7268e9c6
Ryo Midorikawa <midoriiiiii9866@gmail.com>
2018-12-31 17:53:03 +0900

    投稿、ユーザー登録時の画像アップロード機能追加 #4

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
-# Include default devise modules. Others available are:
-# :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable 
+         :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
   has_many :posts
+  has_many :favorites, dependent: :destroy
+  has_many :favorite_posts, through: :favorites, source: :post
   mount_uploader :image, ImageUploader
 end
