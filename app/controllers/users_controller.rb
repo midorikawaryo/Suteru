b4184c8c1b034ecd60306c4369d8d969ac41b283
Ryo Midorikawa <midoriiiiii9866@gmail.com>
2019-01-18 08:50:26 +0900

    エラーページ実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @user_posts = @user.posts
+    @posts_count = Post.where(id: @user_posts).count
   end
 
   def edit
