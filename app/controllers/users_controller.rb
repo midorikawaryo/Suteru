6b33cb6a097eea7583c3833aa537df98d94f1d81
Ryo Midorikawa <midoriiiiii9866@gmail.com>
2019-01-19 16:48:37 +0900

    投稿一覧、お気に入りページ、ユーザー詳細ページレイアウト編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @user_posts = @user.posts
     @posts_count = Post.where(id: @user_posts).count
-    logger.debug("ユーザーズコントローラーshow")
   end
 
   def edit
     @user = User.find(params[:id])
-    logger.debug("ユーザーズコントローラーedit")
   end
   
 end
