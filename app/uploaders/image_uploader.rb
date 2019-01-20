445a774f0c2aca51904e6fb61263367db6e3fd37
Ryo Midorikawa <midoriiiiii9866@gmail.com>
2019-01-20 12:05:27 +0900

    AWS S3への画像アップロード設定

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -44,4 +44,25 @@ class ImageUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
+
+  if Rails.env.development?
+    storage :file
+  elsif Rails.env.test?
+    storage :file
+  else
+    storage :fog
+  end
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def extension_whitelist
+    %w(png jpg)
+  end
+
+  def filename
+    original_filename if original_filename
+  end
+
 end
